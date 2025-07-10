9c0af2605772cb4a85e14095dc305841d08967ab
Egor <business.xaf@gmail.com>
2025-07-10 21:52:45 +0600

    build: init

---
+++ Shikaru.SOC/Core/ModConstant.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Shikaru.SOC.Core
+{
+    public static class ModConstant
+    {
+        public const String Id = "Shikaru.SOC";
+    }
+}
---
+++ Shikaru.SOC/Entrypoint.cs
@@ -0,0 +1,48 @@
+﻿using BepInEx;
+using HarmonyLib;
+using Shikaru.SOC.Core;
+using System;
+using System.Reflection;
+
+namespace Shikaru.SOC
+{
+    [BepInPlugin(ModConstant.Id, "Shikaru SOC", "1.0.0.0")]
+    public class Entrypoint : BaseUnityPlugin
+    {
+        void Awake()
+        {
+            try
+            {
+                Logger.LogMessage("Initializing...");
+
+                PatchMethods();
+
+                Logger.LogMessage("Successfully initialized!");
+            }
+            catch(Exception ex) 
+            {
+                    Logger.LogError($"Failed to initialize the mod: {ex}");
+            }
+        }
+
+        private void PatchMethods()
+        {
+            try
+            {
+                Logger.LogInfo("[Harmony] Patching methods...");
+                Harmony harmony = new Harmony(ModConstant.Id);
+                Assembly assembly = Assembly.GetExecutingAssembly();
+                foreach (var type in AccessTools.GetTypesFromAssembly(assembly))
+                {
+                    PatchClassProcessor processor = harmony.CreateClassProcessor(type);
+                    if (processor.Patch()?.Count > 0)
+                        Logger.LogInfo($"[Harmony] {type.Name} successfully applied.");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed to patch methods.", ex);
+            }
+        }
+    }
+}
